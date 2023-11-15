b97cc4e981adfddb58d8cd6aee8564957cc5675e
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2023-11-16 01:01:20 +0200

    added Todos.tsx(component) and todo.ts(model)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo('learn React'),
+    new Todo('learn TypeScript')
+  ]
+
   return (
     <div>
-
+      <Todos items={todos} />
     </div>
   );
 }
