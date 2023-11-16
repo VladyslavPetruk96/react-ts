6486b1cbc9cafcdcdd1a6a7b7378b8ba9b0937d5
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2023-11-16 22:33:28 +0200

    added NewTodo.tsx file, and styles

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,7 @@
+import styles from './TodoItem.module.css';
+
+const TodoItem: React.FC<{text: string}> = (props) => {
+  return <li className={styles.item}>{props.text}</li>
+}
+
+export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
+
+import styles from './Todos.module.css';
 
 const Todos: React.FC<{items: Todo[]}> = (props) => {
   return (
-  <ul>
-    {props.items.map(item => <li key={item.id}>{item.text}</li>) }
+  <ul className={styles.todos}>
+    {props.items.map(item => <TodoItem key={item.id} text={item.text} />) }
   </ul>
   )
 }
